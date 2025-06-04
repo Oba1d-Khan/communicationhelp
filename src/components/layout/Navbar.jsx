5501396550f648f37f5871dad57ef281b1009f94
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2025-06-04 16:29:27 +0500

    navbar changes

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,7 +2,8 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { Menu, X } from "lucide-react";
+import { Menu, Search, X } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const navLinks = [
   {
@@ -30,23 +31,47 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="bg-neutral-900 shadow-sm sticky top-0 z-50">
-      <div className="container mx-auto px-4 py-4 flex items-center justify-between">
-        {/* Logo */}
-        <Link href="/" className="flex items-center gap-2">
-          <img src="/logo.svg" alt="Logo" className="h-6 w-auto" />
-          <div className="flex flex-col leading-tight">
+    <header className="bg-neutral-900 shadow-sm sticky top-0 z-50 px-4">
+      <div className="container mx-auto px-4 py-4 pt-5 flex items-center justify-between">
+        <div className="flex aic gap-5">
+          {/* Logo */}
+          <Link href="/" className="flex items-center gap-2">
+            <img src="/images/logo.png" alt="Logo" className="h-6 w-auto" />
+            {/* <div className="flex flex-col leading-tight">
             <span className="font-semibold text-sm text-white">
               How Communication Works
             </span>
             <span className="text-xs text-neutral-400">
               Master the social world
             </span>
+          </div> */}
+          </Link>
+
+          {/* Search bar */}
+
+          <div className="hidden lg:flex items-center md:space-x-4">
+            <div className="relative">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                <button
+                  type="submit"
+                  title="Search"
+                  className="p-1 focus:outline-none focus:ring cursor-pointer"
+                >
+                  <Search size={20} strokeWidth="2.2" />
+                </button>
+              </span>
+              <input
+                type="search"
+                name="Search"
+                placeholder="Search..."
+                className="w-32 px-3 py-2 pl-10 text-sm rounded-full sm:w-auto focus:outline-none dark:bg-gray-100 dark:text-gray-800 focus:dark:bg-gray-50"
+              />
+            </div>
           </div>
-        </Link>
+        </div>
 
         {/* Desktop Nav */}
-        <nav className="hidden md:flex gap-6 items-center">
+        <nav className="hidden lg:flex gap-6 items-center">
           {navLinks.map((link) => (
             <Link
               key={link.title}
@@ -56,9 +81,11 @@ const Navbar = () => {
               {link.title}
             </Link>
           ))}
+          <span className="hidden md:block w-px h-6 bg-gray-400 "></span>
+
           <Button
             variant="outline"
-            className="rounded-full border-white text-black hover:bg-white hover:text-black transition"
+            className="cursor-pointer rounded-full border-white text-black hover:bg-white hover:text-black transition"
           >
             Recommended Readings
           </Button>
@@ -66,34 +93,73 @@ const Navbar = () => {
 
         {/* Mobile Toggle */}
         <button
-          className="md:hidden"
+          className="lg:hidden "
           onClick={() => setIsOpen((prev) => !prev)}
         >
-          {isOpen ? <X size={24} /> : <Menu size={24} />}
+          {isOpen ? (
+            <X size={26} color="#ffffff" />
+          ) : (
+            <Menu size={26} color="#ffffff" />
+          )}
         </button>
       </div>
 
       {/* Mobile Nav */}
-      {isOpen && (
-        <div className="md:hidden bg-neutral-800 text-white px-4 pb-4 space-y-3">
-          {navLinks.map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="block text-sm font-medium tracking-widest uppercase hover:text-blue-400 transition-colors"
-              onClick={() => setIsOpen(false)}
-            >
-              {link.title}
-            </Link>
-          ))}
-          <Button
-            variant="outline"
-            className="w-full rounded-full border-white text-black hover:bg-white hover:text-black transition"
+
+      <AnimatePresence>
+        {isOpen && (
+          <motion.nav
+            key="mobile-nav"
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -100, opacity: 0 }}
+            transition={{ type: "tween", ease: "easeInOut", duration: 0.4 }}
+            className="fixed  left-0 w-full h-screen z-40 bg-neutral-900 lg:hidden"
           >
-            Recommended Readings
-          </Button>
-        </div>
-      )}
+            {/* Search bar */}
+            <div className="w-full flex justify-center py-10 px-4">
+              <div className="relative w-[80vw] max-w-md mx-auto">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                  <button
+                    type="submit"
+                    title="Search"
+                    className="p-2 focus:outline-none focus:ring cursor-pointer"
+                  >
+                    <Search size={20} strokeWidth="2.2" />
+                  </button>
+                </span>
+                <input
+                  type="search"
+                  name="Search"
+                  placeholder="Search..."
+                  className="w-full px-3 py-2 pl-13 text-base rounded-full focus:outline-none dark:bg-gray-100 dark:text-gray-800 focus:dark:bg-gray-50"
+                />
+              </div>
+            </div>
+
+            {/* Nav links */}
+            <div className="px-12 text-center space-y-8">
+              {navLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="block text-base font-medium tracking-widest uppercase text-white hover:text-blue-400 transition-colors"
+                  onClick={() => setIsOpen(false)}
+                >
+                  {link.title}
+                </Link>
+              ))}
+
+              <Button
+                variant="outline"
+                className="w-[80vw]  max-w-md  mt-5 rounded-full border-white text-base text-black hover:bg-white hover:text-black transition"
+              >
+                Recommended Readings
+              </Button>
+            </div>
+          </motion.nav>
+        )}
+      </AnimatePresence>
     </header>
   );
 };
