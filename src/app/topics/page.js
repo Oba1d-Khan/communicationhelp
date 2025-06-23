8ff461fc5bc462f7e59ebff9490d74b6a68d5bce
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2025-06-23 16:09:19 +0500

    changes

--- src/app/topics/page.js
+++ src/app/topics/page.js
@@ -1,6 +1,8 @@
+import LoaderGradient from "@/components/shared/LoaderGradient";
 import TopicsList from "@/components/topics/topics-list";
 import { client } from "@/sanity/client";
 import { ALL_BLOGS_QUERY, ALL_TOPICS_QUERY } from "@/sanity/utils/queries";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Topics | Bruce Lambert - Communication & Persuasion",
@@ -16,42 +18,10 @@ export default async function TopicsPage() {
   const topics = await client.fetch(ALL_TOPICS_QUERY, options);
   return (
     <main className="root-layout ">
-      {/* Hero Section */}
-      {/* <section className="relative pt-10 md:pt-6 md:pb-2 overflow-hidden">
-        <div className=" relative z-10">
-          <div className="max-w-3xl mx-auto text-center">
-            <h1 className=" text-5xl font-heading text-foreground mb-4">
-              Explore Topics
-            </h1>
-            <p className="text-lg text-text-light leading-relaxed mx-4">
-              Dive deep into the essential skills of human connection. Choose a
-              topic below to discover insights, techniques, and real-world
-              applications that will transform your communication.
-            </p>
-          </div>
-        </div>
-      </section> */}
-
-      {/* Topics Grid */}
-      {/* <section className="pb-20 md:pb-32">
-        <div className="">
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
-            {topics.map((topic) => (
-              <TopicCard
-                key={topic.id}
-                topic={topic.topic}
-                icon={topic.icon}
-                description={topic.description}
-                href={topic.href}
-                gradient={topic.gradient}
-              />
-            ))}
-          </div>
-        </div>
-      </section> */}
-
       <section>
-        <TopicsList blogs={blogs} topics={topics} />
+        <Suspense fallback={<LoaderGradient />}>
+          <TopicsList blogs={blogs} topics={topics} />
+        </Suspense>
       </section>
     </main>
   );
