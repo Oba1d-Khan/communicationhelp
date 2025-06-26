fe8a7911da20e134b5538996778a497e8fcd3385
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2025-06-26 18:28:09 +0500

    changes

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://communicationhelp.vercel.app",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  exclude: ["/sanity"],
+};
